3cf2ce65601733ae76eb7aaf2f513ef70727d7d0
Dixit Gogoi <dixitgogoi2017@gmail.com>
2023-09-15 23:42:12 +0530

    active class style added

--- src/components/SearchResultHeader.jsx
+++ src/components/SearchResultHeader.jsx
@@ -33,9 +33,9 @@ const SearchResultHeader = () => {
 
             <div className="flex ml-[-12px] mt-2">
                 {menu.map((menu, index) => (
-                    <span key={index} className={`flex items-center p-3 text-[#5f6368] cursor-pointer relative ${selectedMenu === menu.name ? "text-[#1a73e8]" : ""}`} onClick={() => clickHandler(menu)}>
-                        <span className="hidden md:block mr-[5px]">{menu.icon}</span>
-                        <span className="text-sm">{menu.name}</span>
+                    <span key={index} className="flex items-center p-3 text-[#5f6368] cursor-pointer relative" onClick={() => clickHandler(menu)}>
+                        <span className={`hidden md:block mr-[5px] ${selectedMenu === menu.name ? "text-[#1a73e8]" : ""}`}>{menu.icon}</span>
+                        <span className={`text-sm ${selectedMenu === menu.name ? "text-[#1a73e8]" : ""}`}>{menu.name}</span>
                         {selectedMenu === menu.name && (
                             <span className="h-[3px] w-[calc(100%-20px)] absolute bg-[#1a73e8] bottom-0 left-[10px]"></span>
                         )}
